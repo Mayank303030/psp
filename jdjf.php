712789bc8392777044b96146e7da6381ad03f39e
Mayank303030 <92243054+Mayank303030@users.noreply.github.com>
2023-03-10 16:16:50 +0000

    Add files via upload

---
+++ jdjf.php
@@ -0,0 +1,50 @@
+<?php
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the form data
+  $title = $_POST['title'];
+  $author = $_POST['author'];
+  $category = $_POST['category'];
+  $story = $_POST['story'];
+
+  // Validate the form data
+  $errors = array();
+  if (empty($title)) {
+    $errors[] = 'Please enter a title.';
+  }
+  if (empty($author)) {
+    $errors[] = 'Please enter an author.';
+  }
+  if (empty($category)) {
+    $errors[] = 'Please select a category.';
+  }
+  if (empty($story)) {
+    $errors[] = 'Please enter a story.';
+  }
+
+  // If there are no errors, add the story to the database or file
+  if (empty($errors)) {
+    // Add the story to the database or file
+    // ...
+
+    // Redirect the user to the homepage or the story page
+    header('Location: index.php');
+    exit;
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <?php include('header.php'); ?>
+    <div class="content">
+      <h1>Add a Story</h1>
+      <?php if (!empty($errors)): ?>
+        <div class="errors">
+          <?php foreach ($errors as $error): ?>
+            <
